a723870ffde779963aef7e5c0553cb38077b2336
Francisse99 <142090161+Francisse99@users.noreply.github.com>
2025-09-15 18:18:02 +0300

    Delete script directory

--- script/accueil.js
+++
@@ -1,9 +0,0 @@
-function reel() {
-  document.getElementById("reel").style.display = "block";
-}
-
-function show() {
-  document.getElementById("none").style.display = "block";
-  let video = document.getElementById("dmdvideo");
-  video.autoplay = "true";
-}
